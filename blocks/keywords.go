de4bd1128866ecf1b2e252a2ab64e8f6e9725d81
alexdenkk <alexdenkk@yandex.ru>
2024-03-05 16:42:20 +0700

    -

---
+++ blocks/keywords.go
@@ -0,0 +1,14 @@
+package blocks
+
+var KEYWORDS = map[string]string{
+	// math operators
+	"plus":           "+",
+	"minus":          "-",
+	"multiplication": "*",
+	"division":       "/",
+	"exact-division": "//",
+
+	// variables assignment
+	"default-assignment": "@=",
+	"special-assignment": "@@=",
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module mnb/mnb-lang
+
+go 1.21.4
